096d3c4f0cec14715422d3796317c626f87e9edb
Moe Farid <moe.faridd@gmail.com>
2021-05-27 23:33:55 -0400

    3

--- src/Box.js
+++ src/Box.js
@@ -33,6 +33,12 @@ function Box() {
           src='https://www.rollingstone.com/wp-content/uploads/2020/02/james-hetfield-canceled-shows.jpg'
         />
       </div>
+      <div>
+        <img
+          className='Hetfield'
+          src='https://www.rollingstone.com/wp-content/uploads/2020/02/james-hetfield-canceled-shows.jpg'
+        />
+      </div>
     </div>
   )
 }
